76471372908a35c84941022186aeec7902a93cb6
Mayank-Chaudhary <mchaudhary0000@gmail.com>
2024-10-05 23:06:05 +0530

    undo my cors

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ import cors from 'cors'
 
 const app = express();
 dotenv.config()
-const corsOptions = {
-    origin: 'https://demohfhsite.netlify.app/', // allow only your Netlify domain
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // specify the HTTP methods allowed
-    credentials: true, // enable Access-Control-Allow-Credentials
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  };
+// const corsOptions = {
+//     origin: 'https://demohfhsite.netlify.app/', // allow only your Netlify domain
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // specify the HTTP methods allowed
+//     credentials: true, // enable Access-Control-Allow-Credentials
+//     preflightContinue: false,
+//     optionsSuccessStatus: 204,
+//   };
   
-  app.use(cors(corsOptions));
+//   app.use(cors(corsOptions));
+  app.use(cors())
 app.use(express.json());
-// app.use(cors())
 app.use('/',routes)
 
 
